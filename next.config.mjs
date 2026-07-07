edad0d464c55cdf82f4c92267efe6a8aa08726fd
Eugen BKU <eugenbku@gmail.com>
2026-07-07 22:42:41 +0300

    Fix image remote patterns for Vercel Blob and remove duplicate config

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // This setting ensures that your build will not fail due to 
-    // minor ESLint warnings or unescaped entity errors.
     eslint: {
         ignoreDuringBuilds: true,
     },
-    // If you are using next/image with external domains, 
-    // you might eventually need to add them here:
     images: {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: '**',
+                hostname: '*.public.blob.vercel-storage.com',
+                port: '',
+                pathname: '/**',
             },
+            // If you have other external image hosts, add them here
         ],
     },
 };
